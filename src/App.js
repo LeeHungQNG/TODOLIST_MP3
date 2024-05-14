de8b56d7d026a5462205580fdc64cc248a2a63ce
LeeHungQNG <builequochung3004@gmail.com>
2024-05-14 20:44:48 +0700

    v6

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Home, Login, Public } from './containers/public';
+import { Route, Routes } from 'react-router-dom';
+import path from './utils/path';
 
 function App() {
-  const dispatch = useDispatch();
-  const { test } = useSelector((state) => state.app);
-  console.log('🚀 ~ App ~ test:', test);
   return (
     <>
-      <div className=""></div>
+      <div className="">
+        <Routes>
+          {/* Nested router */}
+          <Route path={path.PUBLIC} element={<Public></Public>}>
+            {/* Route con mặc định lấy path của cha nằm ngoài / */}
+            {/* <Route path="home" element={<Home></Home>} /> */}
+            <Route path={path.HOME} element={<Home></Home>} />
+            <Route path={path.LOGIN} element={<Login></Login>} />
+          </Route>
+        </Routes>
+      </div>
       <ToastContainer
         position="top-right"
         autoClose={5000}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import reduxConfig from './store';
-
+import { BrowserRouter } from 'react-router-dom';
 const store = reduxConfig(); // Vì reduxConfig trả về một hàm chứ không phải biến nên phải gọi hàm
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
