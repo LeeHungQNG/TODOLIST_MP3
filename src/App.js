cbcca9ed07a34edb0b4624feedfc948da61ab649
LeeHungQNG <builequochung3004@gmail.com>
2024-05-14 08:41:36 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+
 function App() {
-  return <div className="App">
-    
-    text
-    
-    </div>;
+  const [work, setWork] = useState('');
+
+  return (
+    <div className="flex gap-8 h-screen border border-red-300  justify-center items-center">
+      <input type="text" value={work} onChange={(e) => setWork(e.target.value)} className="outline-none border border-blue-600 px-4 py-2 w-[400px]" />
+      <button type="button" className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white">
+        Add
+      </button>
+    </div>
+  );
 }
 
 export default App;
